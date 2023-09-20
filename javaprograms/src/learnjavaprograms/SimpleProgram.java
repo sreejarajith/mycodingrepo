012e7a055fb845c0fce6f6f7272dc6a7541d6245
Sreeja Rajith <Sreeja Rajith@10.0.0.73>
2023-09-20 17:26:13 -0600

    commit2

---
+++ javaprograms/src/learnjavaprograms/SimpleProgram.java
@@ -0,0 +1,13 @@
+package learnjavaprograms;
+
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;int b=34;
+
+int result=a-b;
+System.out.println(result);
+	}
+
+}
---
+++ javaprograms/src/learnjavaprograms/Subtraction.java
@@ -0,0 +1,12 @@
+package learnjavaprograms;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SimpleProgram prog=new SimpleProgram();
+prog.main(args);
+
+	}
+
+}
